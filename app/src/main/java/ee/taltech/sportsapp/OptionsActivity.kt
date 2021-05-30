3abd39f797d322fd7bf5a8fed206078c9d0111b0
Robert <roaltm@ttu.ee>
2021-05-30 23:03:38 +0300

    Add login and register POST requests, token saving locally

--- app/src/main/java/ee/taltech/sportsapp/OptionsActivity.kt
+++ app/src/main/java/ee/taltech/sportsapp/OptionsActivity.kt
@@ -1,16 +1,62 @@
 package ee.taltech.sportsapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import ee.taltech.sportsapp.other.Constants
 import kotlinx.android.synthetic.main.activity_options.*
+import org.json.JSONObject
 
 class OptionsActivity : AppCompatActivity() {
+
+    var logtag = "OptionsActivity"
+
+    lateinit var email: String
+    lateinit var password: String
+    lateinit var lastname: String
+    lateinit var firstname: String
+
+    lateinit var token: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
+
+        buttonLogIn.setOnClickListener {
+            val emailText = findViewById<EditText>(R.id.editTextTextEmailAddress).text.toString()
+            val passwordText = findViewById<EditText>(R.id.editTextTextPassword).text.toString()
+            if (emailText.isNotEmpty() && passwordText.isNotEmpty()) {
+                Log.d(logtag, "Email: $emailText, Password: $passwordText")
+                email = emailText
+                password = passwordText
+                sendLoginRequest()
+            } else {
+                Toast.makeText(this, "Bad input", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        buttonRegister.setOnClickListener {
+            val emailText = findViewById<EditText>(R.id.editTextTextEmailAddressRegister).text.toString()
+            val passwordText = findViewById<EditText>(R.id.editTextTextPasswordRegister).text.toString()
+            val firstNameText = findViewById<EditText>(R.id.editTextTextFirstname).text.toString()
+            val lastNameText = findViewById<EditText>(R.id.editTextTextLastname).text.toString()
+            if (emailText.isNotEmpty() && passwordText.isNotEmpty() && firstNameText.isNotEmpty() && lastNameText.isNotEmpty()) {
+                Log.d(logtag, "Email: $emailText, Password: $passwordText, Firstname: $firstNameText, Lastname: $lastNameText")
+                email = emailText
+                password = passwordText
+                firstname = firstNameText
+                lastname = lastNameText
+                sendRegisterRequest()
+            } else {
+                Toast.makeText(this, "Bad input", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         spinnerGPSInterval.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
@@ -73,5 +119,72 @@ class OptionsActivity : AppCompatActivity() {
         }
     }
 
+    private fun sendLoginRequest() {
+        val queue = Volley.newRequestQueue(this)
+        val url = Constants.BASEURL + "account/login"
+        Log.d(logtag, url)
 
+        val params = HashMap<String,String>()
+        params["email"] = email
+        params["password"] = password
+        val jsonObject = JSONObject(params as Map<*, *>)
+
+        val request = object: JsonObjectRequest(
+            Method.POST,url,jsonObject,
+            { response ->
+                try {
+                    Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show()
+                    token = response.getString("token")
+                }catch (e:Exception){
+                    Log.d(logtag, e.toString())
+                }
+            }, {
+                Log.d(logtag, "Error in request")
+                Toast.makeText(this, "Error in request", Toast.LENGTH_SHORT).show()
+            })
+
+        {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-Type"] = "application/json"
+                return headers
+            }
+        }
+        queue.add(request)
+    }
+
+    private fun sendRegisterRequest() {
+        val queue = Volley.newRequestQueue(this)
+        val url = Constants.BASEURL + "account/register"
+
+        val params = HashMap<String,String>()
+        params["email"] = email
+        params["password"] = password
+        params["firstName"] = firstname
+        params["lastName"] = lastname
+        val jsonObject = JSONObject(params as Map<*, *>)
+        Log.d(logtag, jsonObject.toString(4))
+
+        val request = object: JsonObjectRequest(Method.POST,url,jsonObject,
+            { response ->
+                try {
+                    Toast.makeText(this, "Registered", Toast.LENGTH_SHORT).show()
+                    token = response.getString("token")
+                }catch (e:Exception){
+                    Log.d(logtag, e.toString())
+                }
+            }, {
+                Log.d(logtag, "Error in request")
+                Toast.makeText(this, "Error in request", Toast.LENGTH_SHORT).show()
+
+            })
+        {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-Type"] = "application/json"
+                return headers
+            }
+        }
+        queue.add(request)
+    }
 }
